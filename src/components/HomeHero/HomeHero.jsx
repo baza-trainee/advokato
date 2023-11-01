9b665bcd01fe622d5786dd55ea04330c8c6f9b3b
Yevhenii2022 <3starosta3@gmail.com>
2023-11-01 18:42:56 +0200

    feat: add general styles

--- src/components/HomeHero/HomeHero.jsx
+++ src/components/HomeHero/HomeHero.jsx
@@ -1,21 +1,23 @@
-import { SectionStyled, Container } from './HomeHero.styled';
+import { SectionStyled, Container, ButtonMain, ButtonSecondary } from "./HomeHero.styled";
 
 export const HomeHero = () => {
-  return (
-    <SectionStyled>
-      <Container>
-        <h1>Довіра, досвід, результат</h1>
-        <ul>
-          <li>
-            Ми допомагаємо приватним клієнтам, державним компаніям, малому і
-            великому бізнесу вирішувати юридичні питання будь-якої складності.
-          </li>
-          <li>
-            Ми допомагаємо передбачати необхідні зміни і уникати таким чином
-            складних ситуацій в майбутньому.
-          </li>
-        </ul>
-      </Container>
-    </SectionStyled>
-  );
+	return (
+		<SectionStyled>
+			<Container>
+				<h1>Довіра, досвід, результат</h1>
+				<ul>
+					<li>
+						Ми допомагаємо приватним клієнтам, державним компаніям, малому і великому бізнесу
+						вирішувати юридичні питання будь-якої складності.
+					</li>
+					<li>
+						Ми допомагаємо передбачати необхідні зміни і уникати таким чином складних ситуацій в
+						майбутньому.
+					</li>
+				</ul>
+				<ButtonMain>Записатися на консультацію</ButtonMain>
+				<ButtonSecondary>Корпоративне право</ButtonSecondary>
+			</Container>
+		</SectionStyled>
+	);
 };
--- src/styles/mixins.js
+++ src/styles/mixins.js
@@ -1,48 +1,125 @@
 export const flexBox = () => {
-  return `display: flex;
+	return `display: flex;
         justify-content: center;
         align-items: center;`;
 };
 
 export const container = () => {
-  return `padding: 0 60px;`;
+	return `width: 100%;
+        margin: 0 auto;
+        padding: 0 20px;
+    
+        @media screen and (min-width: 768px) {
+          padding: 0 40px;
+        }
+
+        @media screen and (min-width: 1440px) {
+          max-width: 1440px;
+          padding: 0 160px;
+        }`;
 };
 
-export const button = () => {
-  return `height: 40px;
-        width: 100%;
-  
-        font-size: 24px;
-        font-weight: 700;
-        line-height: 1.3;
-        text-transform: uppercase;
+export const mainButton = () => {
+	return `  
+        padding: 20px 32px;
+        color: #ffffff;
+        background-color: var(--darkBackground);
+        cursor: pointer;
+        border: 1px solid var(--greyText);
+        font-family: inherit;
+        font-size: 18px;
+        font-weight: 500;
+        line-height: calc(22 / 18);
+        text-align: center;
+        border-radius: 48px;
+        transition: color var(--timing-function) var(--animation-duration), border var(--timing-function) var(--animation-duration), background-color var(--timing-function) var(--animation-duration);
+
+        &:hover,
+        &:focus {
+          color: var(--mainText);
+          background-color: #ffffff;
+          border: 1px solid #ffffff;
+        }
+
+        &:active {
+          color: var(--mainText);
+          background-color: #e1e1e1; 
+          border: 1px solid #e1e1e1;
+        }
+
+        &[disabled] {
+          color: var(--greyText);   
+          cursor: not-allowed;
+        
+        &:hover,
+        &:focus {
+          color: var(--greyText);
+          background-color: var(--darkBackground);
+          border: 1px solid var(--greyText);
+          }
+        }
+    `;
+};
+
+export const secondaryButton = () => {
+	return `  
+        padding: 12px 24px;
+        color: var(--lightText);
+        background-color: #1c1c21;
+        cursor: pointer;
         border: none;
+        font-family: inherit;
+        font-size: 14px;
+        font-weight: 500;
+        line-height: calc(17 / 14);
+        text-align: center;
+        border-radius: 50px;
+        transition: color var(--timing-function) var(--animation-duration), background-color var(--timing-function) var(--animation-duration);
+
+        &:hover,
+        &:focus {
+          color: var(--mainText);
+          background-color: #ffffff;        
+        }
+
+        &:active {
+          color: var(--mainText);
+          background-color: #e1e1e1;        
+        }
+
+        &[disabled] {
+          color: var(--greyText);   
+          cursor: not-allowed;
+        
+        &:hover,
+        &:focus {
+          color: var(--greyText);
+          background-color: #1c1c21;          
+          }
+        }
     `;
 };
 
 export const fontDesktop = () => {
-  return `
-        font-size: 24px;
-        font-weight: 700;
-        line-height: 1.3;
-        text-transform: uppercase;
+	return `
+        font-size: 22px;
+        font-weight: 400;
+        line-height: calc(35 / 22);      
     `;
 };
 
 export const fontTablet = () => {
-  return `
+	return `
         font-size: 20px;
         font-weight: 500;
-        line-height: 1.3;
-        text-transform: uppercase;
+        line-height: 1.3;        
     `;
 };
 
 export const fontMobile = () => {
-  return `
+	return `
         font-size: 14px;
         font-weight: 400;
-        line-height: 1.3;
-        text-transform: uppercase;
+        line-height: 1.3;        
     `;
 };
