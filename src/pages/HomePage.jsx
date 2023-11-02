c0f2ce5299a259cbdffc23f4f25fdfcaf2424dac
Yevhenii2022 <3starosta3@gmail.com>
2023-11-02 09:33:50 +0200

    feat: add main structure Info section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,27 +1,28 @@
-import { HomeHero } from '../components/HomeHero';
+import { HomeHero } from "../components/HomeHero";
+import { Information } from "../components/Information";
 
 export const HomePage = () => {
-  return (
-    <>
-      <HomeHero />
+	return (
+		<>
+			<HomeHero />
 
-      <section>Company main section</section>
+			<section>Company main section</section>
 
-      <section>Company second section</section>
+			<Information />
 
-      <section>Practice section</section>
+			<section>Practice section</section>
 
-      <section>Team section</section>
+			<section>Team section</section>
 
-      <section>ProBono section</section>
+			<section>ProBono section</section>
 
-      <section>Achievement section</section>
+			<section>Achievement section</section>
 
-      <section>Clients section</section>
+			<section>Clients section</section>
 
-      <section>FeedBack section</section>
-    </>
-  );
+			<section>FeedBack section</section>
+		</>
+	);
 };
 
 export default HomePage;
