0db205a81833bc386d7e1ccedd1d8742a3375c93
Wolodymyr Magera <foxwest@ukr.net>
2023-11-02 10:59:43 +0200

    header&HomeHeroPage

--- src/components/HomeHero/HomeHero.jsx
+++ src/components/HomeHero/HomeHero.jsx
@@ -1,22 +1,10 @@
-import { SectionStyled, Container, ButtonMain, ButtonSecondary } from "./HomeHero.styled";
+import { SectionStyled, Container} from "./HomeHero.styled";
 
 export const HomeHero = () => {
 	return (
 		<SectionStyled>
 			<Container>
-				<h1>Довіра, досвід, результат</h1>
-				<ul>
-					<li>
-						Ми допомагаємо приватним клієнтам, державним компаніям, малому і великому бізнесу
-						вирішувати юридичні питання будь-якої складності.
-					</li>
-					<li>
-						Ми допомагаємо передбачати необхідні зміни і уникати таким чином складних ситуацій в
-						майбутньому.
-					</li>
-				</ul>
-				<ButtonMain>Записатися на консультацію</ButtonMain>
-				<ButtonSecondary>Корпоративне право</ButtonSecondary>
+				
 			</Container>
 		</SectionStyled>
 	);
