af093191dcf088009fc5bdd59fe8bcb4ce49d84b
Gregory <bjoldgv@gmail.com>
2023-11-02 19:59:43 +0200

    Fix styles

--- src/components/Owner/Owner.styled.js
+++ src/components/Owner/Owner.styled.js
@@ -1,11 +1,9 @@
 import styled from "@emotion/styled";
-import {
-  container,
-  secondaryButton,
-} from "../../styles/mixins";
+import { container, secondaryButton } from "../../styles/mixins";
 import "../../index.css";
 
 export const SectionStyled = styled.section`
+  height:100vh;
   background-color: var(--darkBackground);
   color: var(--white);
 `;
@@ -17,12 +15,20 @@ export const Container = styled.div`
   justify-content: space-between;
   padding: 0 !important;
   max-width: inherit !important;
+
+  @media screen and (min-width: 1440px){
+    padding: 0;
+  }
+  @media screen and (min-width: 768px){
+    padding: 0;
+  }
+
 `;
 
 export const TextStyled = styled.div`
   h3 {
     font-family: "Raleway";
-    font-size: clamp(18px,2vw,32px);
+    font-size: clamp(18px, 2vw, 32px);
     font-weight: 400;
     line-height: 45px;
     letter-spacing: 0em;
@@ -31,7 +37,7 @@ export const TextStyled = styled.div`
 
   span {
     font-family: "Raleway";
-    font-size: clamp(32px,3.5vw,64px);
+    font-size: clamp(32px, 3.5vw, 64px);
     font-weight: 700;
     line-height: 90px;
     letter-spacing: 0em;
@@ -40,13 +46,16 @@ export const TextStyled = styled.div`
 
   p {
     font-family: "PT Sans";
-    font-size: clamp(14px,1.2vw,22px);
+    font-size: clamp(14px, 1.2vw, 22px);
     font-weight: 400;
     line-height: 35px;
     letter-spacing: 0em;
     text-align: left;
     width: 484px;
     margin: 40px 0;
+  }
+  @media screen and (min-width: 768px) {
+    padding:  40px;
   }
 `;
 
@@ -60,11 +69,13 @@ export const CompanyBox = styled.div`
 
 export const ImageStyled = styled.img`
   display: block;
-  background-image: url(/public/images/photo-owner.jpg);
+  background-image: url("/src/components/Owner/img/photo-owner.jpg");
   background-repeat: no-repeat;
   object-fit: cover;
+  -webkit-background-size: cover;
+  background-size: cover;
   min-width: 611px;
-  ${'' /* min-width: clamp(200px, 30vw, 611px); */}
+  width: clamp(200px, 35vw, 611px);
   height: auto;
 `;
 
@@ -73,7 +84,7 @@ export const ButtonStyled = styled.button`
   background: var(--darkBackground);
   border: 1px solid var(--white);
   font-family: "Raleway";
-  font-size: clamp(12px,1.4vw,24px);
+  font-size: clamp(12px, 1.4vw, 24px);
   font-weight: 600;
   line-height: 27px;
   letter-spacing: 0em;
