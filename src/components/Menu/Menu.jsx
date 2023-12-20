7820bad45b5c1e15f8bc9a8cbfa34ae454942a95
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-12-21 01:12:15 +0200

    fix active color in menu header and burger

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,33 +1,86 @@
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { NavLinkStyled, UlStyled } from './Menu.styled';
 import { useTranslation } from 'react-i18next';
 
 export const Menu = ({ side }) => {
   const [t, i18n] = useTranslation('global');
+  const [active, setActive] = useState('home');
+  const { pathname, hash } = useLocation();
+
+  useEffect(() => {
+    if (pathname === '/' && hash === '') {
+      return setActive(prev => 'home');
+    }
+    if (pathname === '/company') {
+      return setActive(prev => 'company');
+    }
+    if (hash === '#practice') {
+      return setActive(prev => 'practice');
+    }
+    if (hash === '#news') {
+      return setActive(prev => 'news');
+    }
+    if (pathname === '/contacts') {
+      return setActive(prev => 'contacts');
+    }
+  }, [pathname, hash]);
+
+  const handleClickNavButton = ({ target }) => {
+    console.log(target.getAttribute('aria-label'));
+    setActive(prev => target.getAttribute('aria-label'));
+  };
+
   return (
     <nav>
-      <UlStyled sideMenu={side}>
+      <UlStyled sideMenu={side} onClick={handleClickNavButton}>
         <li>
-          <NavLinkStyled to="/" aria-label="home">
+          <NavLinkStyled
+            to="/"
+            aria-label="home"
+            current="home"
+            active={active}
+          >
             {t('header.nav.home')}
           </NavLinkStyled>
         </li>
         <li>
-          <NavLinkStyled to="/company" aria-label="company">
+          <NavLinkStyled
+            to="/company"
+            aria-label="company"
+            current="company"
+            active={active}
+          >
             {t('header.nav.company')}
           </NavLinkStyled>
         </li>
         <li>
-          <NavLinkStyled to="/#practice" aria-label="practice">
+          <NavLinkStyled
+            to="/#practice"
+            aria-label="practice"
+            current="practice"
+            active={active}
+          >
             {t('header.nav.practice')}
           </NavLinkStyled>
         </li>
         <li>
-          <NavLinkStyled to="/#news" aria-label="news">
+          <NavLinkStyled
+            to="/#news"
+            aria-label="news"
+            current="news"
+            active={active}
+          >
             {t('header.nav.news')}
           </NavLinkStyled>
         </li>
         <li>
-          <NavLinkStyled to="/contacts" aria-label="contacts">
+          <NavLinkStyled
+            to="/contacts"
+            aria-label="contacts"
+            current="contacts"
+            active={active}
+          >
             {t('header.nav.contacts')}
           </NavLinkStyled>
         </li>
