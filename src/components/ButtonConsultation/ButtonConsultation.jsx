17ede6884335d6c7a733f4892c43c4b84f448608
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-12-22 02:31:06 +0200

    add some get query to server

--- src/components/ButtonConsultation/ButtonConsultation.jsx
+++ src/components/ButtonConsultation/ButtonConsultation.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
 
+import { getContent } from '../../api';
 import { Modal } from '../Modal';
 import { Icon } from '../Icon';
 import { Calendar } from './Calendar';
@@ -10,6 +11,9 @@ import { ButtonStyled } from './ButtonConsultation.styled';
 export const ButtonConsultation = ({ className, customStyles }) => {
   const [t, i18n] = useTranslation('global');
   const [modalActive, setModalActive] = useState(false);
+  const [specialization, setSpecialization] = useState([]);
+  const [lawyers, setLawyers] = useState([]);
+  const [schedule, setSchedule] = useState([]);
 
   useEffect(() => {
     if (!modalActive) {
@@ -17,6 +21,45 @@ export const ButtonConsultation = ({ className, customStyles }) => {
     }
   }, [modalActive]);
 
+  useEffect(() => {
+    const getData = async () => {
+      // setIsLoading(prev => true);
+      const data = await getContent('lawyer-specs');
+      setSpecialization(prev => data);
+
+      // setIsLoading(prev => false);
+    };
+
+    getData();
+  }, []);
+
+  useEffect(() => {
+    const getData = async () => {
+      // setIsLoading(prev => true);
+      const data = await getContent('lawyers'); //'lawyers?specialization_id=1'
+      setLawyers(prev => data);
+
+      // setIsLoading(prev => false);
+    };
+
+    getData();
+  }, []);
+
+  useEffect(() => {
+    const getData = async () => {
+      // setIsLoading(prev => true);
+      const data = await getContent('schedule?lawyer_id=1');
+      setSchedule(prev => data);
+
+      // setIsLoading(prev => false);
+    };
+
+    getData();
+  }, []);
+
+  console.log(specialization);
+  console.log(lawyers);
+  console.log(schedule);
   return (
     <>
       {modalActive && (
