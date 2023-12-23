6fb5d1186c4262e72d801c2858377dffed9de503
Alexey Yermachkov <o.yermachkov@gmail.com>
2023-12-23 22:44:42 +0200

    refactor cookies-panel: add adptive styles

--- src/components/CookiesPanel/CookiesPanel.styled.js
+++ src/components/CookiesPanel/CookiesPanel.styled.js
@@ -1,37 +1,107 @@
 import styled from "@emotion/styled";
 import { Icon } from "../Icon";
+import { device } from "../../styles/mixins";
 
 export const Panel = styled.div`
 	position: fixed;
-	bottom: 80px;
-	padding: 68px 100px;
-	max-width: 1280px;
+	bottom: 27px;
+	padding: 24px 12px;
+	max-width: 272px;
 
 	background: var(--darkBackground);
 
 	font-family: var(--PTSans);
 	color: var(--lightText);
-	font-size: 22px;
-	line-height: 35px;
+	font-size: 12px;
+	line-height: 19px;
+	text-align: center;
+
+	@media screen and (min-width: 768px) {
+		bottom: 56px;
+		padding: 27px 32px;
+		max-width: 648px;
+
+		font-size: 14px;
+		line-height: 22px;
+		text-align: left;
+	}
+
+	@media screen and (${device.desktop}) {
+		bottom: 88px;
+		padding: 68px 100px;
+		max-width: 1280px;
+
+		font-size: 22px;
+		line-height: 35px;
+	}
+`;
+
+export const PolicyLink = styled.a`
+	font-family: var(--Montserrat);
+	font-size: 12px;
+	line-height: 22px;
+	text-decoration: underline;
+
+	@media screen and (min-width: 768px) {
+		font-size: 14px;
+		line-height: 25px;
+	}
+
+	@media screen and (${device.desktop}) {
+		font-size: 18px;
+		line-height: 32px;
+	}
+`;
+
+export const BannerWrap = styled.div`
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+	gap: 16px;
+
+	@media screen and (min-width: 768px) {
+		flex-direction: row;
+		gap: 67px;
+	}
+
+	@media screen and (${device.desktop}) {
+		gap: 36px;
+	}
 `;
 
 export const IconClose = styled(Icon)`
 	position: absolute;
-	top: 24px;
-	right: 24px;
+	width: 12px;
+	height: 12px;
+	top: 8px;
+	right: 8px;
 	cursor: pointer;
 	stroke: var(--lightText);
+
+	@media screen and (min-width: 768px) {
+		width: 16px;
+		height: 16px;
+		top: 12px;
+		right: 12px;
+	}
+
+	@media screen and (${device.desktop}) {
+		width: 24px;
+		height: 24px;
+		top: 27px;
+		right: 27px;
+	}
 `;
 
 export const ButtonOk = styled.button`
-	min-width: 216px;
-	height: 64px;
+	min-width: 120px;
+	height: 36px;
 	border: 2px solid #fff;
 	border-radius: 5px;
 
 	font-family: var(--Montserrat);
-	font-size: 28px;
-	line-height: 42px;
+	font-size: 16px;
+	line-height: 24px;
 
 	transition: all var(--animation-duration) var(--timing-function);
 
@@ -40,17 +110,15 @@ export const ButtonOk = styled.button`
 		background-color: #fff;
 		color: var(--mainText);
 	}
-`;
 
-export const BannerWrap = styled.div`
-	display: flex;
-	gap: 36px;
-	align-items: center;
-`;
+	@media screen and (min-width: 768px) {
+		min-width: 180px;
+		height: 64px;
+		font-size: 28px;
+		line-height: 42px;
+	}
 
-export const PolicyLink = styled.a`
-	font-family: var(--Montserrat);
-	font-size: 18px;
-	line-height: 32px;
-	text-decoration: underline;
+	@media screen and (${device.desktop}) {
+		min-width: 216px;
+	}
 `;
