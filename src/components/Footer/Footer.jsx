254c22df8b996c99d5f790e4455d26e9c63c5d4f
Yevhenii2022 <3starosta3@gmail.com>
2023-12-23 21:14:19 +0200

    refactor: change active link in the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,172 +1,199 @@
-import { useState, useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
-import { useLocation } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { useLocation } from "react-router-dom";
 
-import { PdfViewer } from '../PdfViewer';
-import { Modal } from '../Modal';
-import { SocialList } from '../SocialList';
+import { PdfViewer } from "../PdfViewer";
+import { Modal } from "../Modal";
+import { SocialList } from "../SocialList";
 import {
-  Container,
-  FlexWraper,
-  FooterStyled,
-  Line,
-  LinkStyled,
-  List,
-  PolicyText,
-  Text,
-  TextLink,
-} from './Footer.styled';
+	Container,
+	FlexWraper,
+	FooterStyled,
+	Line,
+	LinkStyled,
+	List,
+	PolicyText,
+	Text,
+	TextLink,
+} from "./Footer.styled";
 
-import pdfFile from '../../assets/documents/test_privacy_policy.pdf';
-import pdfSiteRules from '../../assets/documents/site_rules.pdf';
-import { getContent } from '../../api/';
+import pdfFile from "../../assets/documents/test_privacy_policy.pdf";
+import pdfSiteRules from "../../assets/documents/site_rules.pdf";
+import { getContent } from "../../api/";
 
 const address =
-  'https://www.google.com.ua/maps/place/%D0%90%D0%B4%D0%B2%D0%BE%D0%BA%D0%B0%D1%82%D1%81%D0%BA%D0%B0%D1%8F+%D0%BA%D0%BE%D0%BC%D0%BF%D0%B0%D0%BD%D0%B8%D1%8F+%22%D0%A1%D0%A2%D0%90%D0%A2%D0%A3%D0%A1%22/@46.9771042,31.9821151,306m/data=!3m1!1e3!4m15!1m8!3m7!1s0x40c5c9736d5787e1:0x22995cbf959306a!2z0YPQuy4g0J3QuNC60L7Qu9GM0YHQutCw0Y8sIDE5LCDQndC40LrQvtC70LDQtdCyLCDQndC40LrQvtC70LDQtdCy0YHQutCw0Y8g0L7QsdC70LDRgdGC0YwsIDU0MDAw!3b1!8m2!3d46.9771042!4d31.9821151!16s%2Fg%2F1tf22ljf!3m5!1s0x40c5c9736d45554d:0xf623d7f2ab386bdb!8m2!3d46.9770991!4d31.9829098!16s%2Fg%2F11hd9rjdw5?entry=ttu';
+	"https://www.google.com.ua/maps/place/%D0%90%D0%B4%D0%B2%D0%BE%D0%BA%D0%B0%D1%82%D1%81%D0%BA%D0%B0%D1%8F+%D0%BA%D0%BE%D0%BC%D0%BF%D0%B0%D0%BD%D0%B8%D1%8F+%22%D0%A1%D0%A2%D0%90%D0%A2%D0%A3%D0%A1%22/@46.9771042,31.9821151,306m/data=!3m1!1e3!4m15!1m8!3m7!1s0x40c5c9736d5787e1:0x22995cbf959306a!2z0YPQuy4g0J3QuNC60L7Qu9GM0YHQutCw0Y8sIDE5LCDQndC40LrQvtC70LDQtdCyLCDQndC40LrQvtC70LDQtdCy0YHQutCw0Y8g0L7QsdC70LDRgdGC0YwsIDU0MDAw!3b1!8m2!3d46.9771042!4d31.9821151!16s%2Fg%2F1tf22ljf!3m5!1s0x40c5c9736d45554d:0xf623d7f2ab386bdb!8m2!3d46.9770991!4d31.9829098!16s%2Fg%2F11hd9rjdw5?entry=ttu";
 
 const currentYear = new Date().getFullYear();
 
 export const Footer = () => {
-  const [t, i18n] = useTranslation('global');
-  const [modalActive, setModalActive] = useState(false);
-  const [selectedPdfFile, setSelectedPdfFile] = useState(null);
-  const location = useLocation();
+	const [t, i18n] = useTranslation("global");
+	const [modalActive, setModalActive] = useState(false);
+	const [active, setActive] = useState("home");
+	const [selectedPdfFile, setSelectedPdfFile] = useState(null);
+	const { pathname, hash } = useLocation();
 
-  // const [cities, setCities] = useState([]);
-  const [contacts, setContacts] = useState([]);
+	// const [cities, setCities] = useState([]);
+	const [contacts, setContacts] = useState([]);
 
-  useEffect(() => {
-    const getData = async () => {
-      const data = await getContent('contacts');
+	useEffect(() => {
+		const getData = async () => {
+			const data = await getContent("contacts");
 
-      // setCities(prev => data.cities);
-      setContacts(prev => data.contacts);
-    };
+			// setCities(prev => data.cities);
+			setContacts(prev => data.contacts);
+		};
 
-    getData();
-  }, []);
+		getData();
+	}, []);
 
-  return (
-    <FooterStyled>
-      <Container>
-        <Modal active={modalActive} setActive={setModalActive}>
-          <PdfViewer pdfFile={selectedPdfFile} />
-        </Modal>
+	useEffect(() => {
+		if (pathname === "/" && hash === "") {
+			return setActive(prev => "home");
+		}
+		if (pathname === "/company") {
+			return setActive(prev => "company");
+		}
+		if (hash === "#practice") {
+			return setActive(prev => "practice");
+		}
+		if (hash === "#news") {
+			return setActive(prev => "news");
+		}
+		if (pathname === "/contacts") {
+			return setActive(prev => "contacts");
+		}
+	}, [pathname, hash]);
 
-        <FlexWraper
-          justifyContent={
-            location.pathname === '/contacts' ? 'space-between' : null
-          }
-        >
-          {location.pathname !== '/contacts' && (
-            <nav>
-              <List>
-                <li>
-                  <LinkStyled to="/" aria-label="посилання на головну сторінку">
-                    {t('header.nav.home')}
-                  </LinkStyled>
-                </li>
-                <li>
-                  <LinkStyled
-                    to="/company"
-                    aria-label="посилання на сторінку компанії"
-                  >
-                    {t('header.nav.company')}
-                  </LinkStyled>
-                </li>
-                <li>
-                  <LinkStyled
-                    to="/#practice"
-                    aria-label="посилання на сторінку практики"
-                  >
-                    {t('header.nav.practice')}
-                  </LinkStyled>
-                </li>
-                <li>
-                  <LinkStyled
-                    to="/#news"
-                    aria-label="посилання на сторінку новин"
-                  >
-                    {t('header.nav.news')}
-                  </LinkStyled>
-                </li>
-                <li>
-                  <LinkStyled
-                    to="/contacts"
-                    aria-label="посилання на сторінку контактів"
-                  >
-                    {t('header.nav.contacts')}
-                  </LinkStyled>
-                </li>
-              </List>
-            </nav>
-          )}
+	return (
+		<FooterStyled>
+			<Container>
+				<Modal
+					active={modalActive}
+					setActive={setModalActive}
+				>
+					<PdfViewer pdfFile={selectedPdfFile} />
+				</Modal>
 
-          {location.pathname === '/contacts' && (
-            <Text>ADVOCATE COMPANY «STATUS»</Text>
-          )}
+				<FlexWraper>
+					<nav>
+						<List>
+							<li>
+								<LinkStyled
+									to="/"
+									aria-label="посилання на головну сторінку"
+									current="home"
+									active={active}
+								>
+									{t("header.nav.home")}
+								</LinkStyled>
+							</li>
+							<li>
+								<LinkStyled
+									to="/company"
+									aria-label="посилання на сторінку компанії"
+									current="company"
+									active={active}
+								>
+									{t("header.nav.company")}
+								</LinkStyled>
+							</li>
+							<li>
+								<LinkStyled
+									to="/#practice"
+									aria-label="посилання на сторінку практики"
+									current="practice"
+									active={active}
+								>
+									{t("header.nav.practice")}
+								</LinkStyled>
+							</li>
+							<li>
+								<LinkStyled
+									to="/#news"
+									aria-label="посилання на сторінку новин"
+									current="news"
+									active={active}
+								>
+									{t("header.nav.news")}
+								</LinkStyled>
+							</li>
+							<li>
+								<LinkStyled
+									to="/contacts"
+									aria-label="посилання на сторінку контактів"
+									current="contacts"
+									active={active}
+								>
+									{t("header.nav.contacts")}
+								</LinkStyled>
+							</li>
+						</List>
+					</nav>
 
-          <div>
-            <PolicyText
-              to="#"
-              onClick={() => {
-                setModalActive(true);
-                setSelectedPdfFile(pdfFile);
-              }}
-              aria-label="посилання на політику конфіденційності"
-            >
-              Політика конфіденційності
-            </PolicyText>
-            <PolicyText
-              to="#"
-              onClick={() => {
-                setModalActive(true);
-                setSelectedPdfFile(pdfSiteRules);
-              }}
-              aria-label="посилання на умови користування сайтом"
-            >
-              Правила користування сайтом
-            </PolicyText>
-          </div>
+					<div>
+						<PolicyText
+							to="#"
+							onClick={() => {
+								setModalActive(true);
+								setSelectedPdfFile(pdfFile);
+							}}
+							aria-label="посилання на політику конфіденційності"
+						>
+							Політика конфіденційності
+						</PolicyText>
+						<PolicyText
+							to="#"
+							onClick={() => {
+								setModalActive(true);
+								setSelectedPdfFile(pdfSiteRules);
+							}}
+							aria-label="посилання на умови користування сайтом"
+						>
+							Правила користування сайтом
+						</PolicyText>
+					</div>
 
-          {location.pathname !== '/contacts' && (
-            <FlexWraper flexDirection="column" gap="0">
-              <Text>ADVOCATE COMPANY «STATUS»</Text>
-              <TextLink
-                to={address}
-                target="_blank"
-                rel="noopener nofollow noreferrer"
-                aria-label="адреса компанії"
-              >
-                Вул. Нікольська 19, м. Миколаїв, <br /> Україна
-              </TextLink>
-              <TextLink
-                to="tel:+380512377373"
-                aria-label="телефон компанії"
-                marginbottom="0"
-              >
-                +38 (0512) 37 73 73
-              </TextLink>
-              <TextLink to="tel:+380933737303" aria-label="телефон компанії">
-                +38 (093) 373 73 03
-              </TextLink>
-              <TextLink
-                to="mailto:acstatus.mk@gmail.com"
-                aria-label="електронна пошта компанії"
-              >
-                acstatus.mk@gmail.com
-              </TextLink>
-              
-              {contacts?.length > 0 && (
-                <SocialList media={contacts[0]?.social} />
-              )}
-            </FlexWraper>
-          )}
-        </FlexWraper>
+					<FlexWraper
+						flexDirection="column"
+						gap="0"
+					>
+						<Text>ADVOCATE COMPANY «STATUS»</Text>
+						<TextLink
+							to={address}
+							target="_blank"
+							rel="noopener nofollow noreferrer"
+							aria-label="адреса компанії"
+						>
+							Вул. Нікольська 19, м. Миколаїв, <br /> Україна
+						</TextLink>
+						<TextLink
+							to="tel:+380512377373"
+							aria-label="телефон компанії"
+							marginbottom="0"
+						>
+							+38 (0512) 37 73 73
+						</TextLink>
+						<TextLink
+							to="tel:+380933737303"
+							aria-label="телефон компанії"
+						>
+							+38 (093) 373 73 03
+						</TextLink>
+						<TextLink
+							to="mailto:acstatus.mk@gmail.com"
+							aria-label="електронна пошта компанії"
+						>
+							acstatus.mk@gmail.com
+						</TextLink>
 
-        <Line />
-        <Text>{`${currentYear} ADVOCATE COMPANY «STATUS». All rights reserved.`}</Text>
-      </Container>
-    </FooterStyled>
-  );
+						{contacts?.length > 0 && <SocialList media={contacts[0]?.social} />}
+					</FlexWraper>
+				</FlexWraper>
+
+				<Line />
+				<Text>{`${currentYear} ADVOCATE COMPANY «STATUS». All rights reserved.`}</Text>
+			</Container>
+		</FooterStyled>
+	);
 };
--- src/components/Footer/Footer.styled.js
+++ src/components/Footer/Footer.styled.js
@@ -1,110 +1,104 @@
-import { Link, NavLink } from 'react-router-dom';
-import styled from '@emotion/styled';
-import { device } from '../../styles/mixins';
+import { Link, NavLink } from "react-router-dom";
+import styled from "@emotion/styled";
+import { device } from "../../styles/mixins";
 
 export const FooterStyled = styled.footer`
-  background-color: var(--darkBackground);
-  border-radius: 24px 24px 0 0;
-  margin-top: -140px;
+	background-color: var(--darkBackground);
+	border-radius: 24px 24px 0 0;
+	margin-top: -140px;
 `;
 
 export const Container = styled.div`
-  @media screen and (${device.desktop}) {
-    padding-top: 72px;
-    padding-bottom: 28px;
-    max-width: 1120px;
-    margin: 0 auto;
-  }
+	@media screen and (${device.desktop}) {
+		padding-top: 72px;
+		padding-bottom: 28px;
+		max-width: 1120px;
+		margin: 0 auto;
+	}
 `;
 
 export const FlexWraper = styled.div`
-  display: flex;
-  flex-direction: ${props => props.flexDirection || 'row'};
-  gap: ${props => props.gap || '140px'};
-  justify-content: ${props => props.justifyContent || 'center'};
+	display: flex;
+	flex-direction: ${props => props.flexDirection || "row"};
+	gap: ${props => props.gap || "140px"};
+	justify-content: center;
 `;
 
 export const List = styled.ul`
-  display: flex;
-  flex-direction: column;
-  gap: 24px;
+	display: flex;
+	flex-direction: column;
+	gap: 24px;
 `;
 
 export const LinkStyled = styled(NavLink)`
-  font-family: var(--Montserrat);
-  font-size: 18px;
-  line-height: calc(32.4 / 18);
-  color: var(--lightText);
-  position: relative;
-  transition: color 0.3s ease-in-out;
+	font-family: var(--Montserrat);
+	font-size: 18px;
+	line-height: calc(32.4 / 18);
+	color: ${({ current, active }) =>
+		current === active ? "var(--accentLink)" : "var(--lightText)"};
+	position: relative;
+	transition: color 0.3s ease-in-out;
 
-  &:hover,
-  &:focus {
-    color: var(--accentLink);
-  }
+	&:hover,
+	&:focus {
+		color: var(--accentLink);
+	}
 
-  &::before {
-    content: '';
-    position: absolute;
-    bottom: -2px;
-    left: 0;
-    width: 0;
-    height: 2px;
-    background-color: var(--accentLink);
-    transition: width 0.3s ease-in-out;
-  }
+	&::before {
+		content: "";
+		position: absolute;
+		bottom: -2px;
+		left: 0;
+		width: ${({ current, active }) => (current === active ? "100%" : "0")};
+		height: 2px;
+		background-color: var(--accentLink);
+		transition: width 0.3s ease-in-out;
+	}
 
-  &:hover::before {
-    width: 100%;
-  }
-
-  &.active {
-    color: var(--accentLink);
-    &::before {
-      width: 100%;
-    }
-  }
+	&:hover::before {
+		width: 100%;
+	}
 `;
 
 export const Text = styled.p`
-  margin-bottom: 16px;
-  font-family: var(--Montserrat);
-  line-height: calc(32.4 / 18);
-  color: var(--lightText);
+	margin-bottom: 16px;
+	font-family: var(--Montserrat);
+	line-height: calc(32.4 / 18);
+	color: var(--lightText);
 `;
 
 export const TextLink = styled(Link)`
-  margin-bottom: ${props => props.marginbottom || '16px'};
-  font-family: var(--Montserrat);
-  line-height: calc(32.4 / 18);
-  color: var(--lightText);
-  transition: color var(--timing-function) var(--animation-duration);
+	margin-bottom: ${props => props.marginbottom || "16px"};
+	font-family: var(--Montserrat);
+	line-height: calc(32.4 / 18);
+	color: var(--lightText);
+	transition: color var(--timing-function) var(--animation-duration);
 
-  &:hover,
-  &:focus {
-    color: var(--accentLink);
-  }
+	&:hover,
+	&:focus {
+		color: var(--accentLink);
+	}
 `;
 
 export const PolicyText = styled(Link)`
-  display: flex;
-  margin-bottom: 24px;
-  font-family: var(--Montserrat);
-  line-height: calc(32.4 / 18);
-  color: var(--lightText);
-  text-decoration: underline;
-  text-underline-offset: 3.5px;
-  transition: color var(--timing-function) var(--animation-duration);
+	display: flex;
+	margin-bottom: 24px;
+	font-family: var(--Montserrat);
+	line-height: calc(32.4 / 18);
+	color: var(--lightText);
+	text-decoration: underline;
+	text-underline-offset: 3.5px;
+	transition: color var(--timing-function) var(--animation-duration);
 
-  &:hover,
-  &:focus {
-    color: var(--accentLink);
-  }
+	&:hover,
+	&:focus {
+		color: var(--accentLink);
+	}
 `;
 
 export const Line = styled.div`
-  margin-bottom: 28px;
-  width: 100%;
-  height: 1px;
-  background-color: var(--lightText);
+	margin-bottom: 28px;
+	width: 100%;
+	height: 1px;
+	background-color: var(--lightText);
 `;
