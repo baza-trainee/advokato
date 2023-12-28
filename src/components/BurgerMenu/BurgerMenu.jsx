308368e9f29d1134f31a952dbb9834dda58a3e42
Wolodymyr Magera <foxwest@ukr.net>
2023-12-29 01:55:25 +0200

    styles changed

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -2,7 +2,7 @@ import { SectionStyled } from "./BurgerMenu.styled";
 import { Menu } from "../Menu";
 
 import { LangButton } from "../Layout/Layout.styled";
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 
 export const BurgerMenu = ({
 	activeLang,
@@ -11,17 +11,6 @@ export const BurgerMenu = ({
 	setIsOpen,
 }) => {
 	const ref = useRef();
-	useEffect(() => {
-		function ClickOut(e) {
-			if (!ref.current.contains(e.target)) {
-				setIsOpen(false);
-			}
-		}
-		document.body.addEventListener("mousedown", ClickOut);
-		return () => {
-			document.removeEventListener("mousedown", ClickOut);
-		};
-	}, []);
 	return (
 		<SectionStyled
 			ref={ref}
