6fe784f71658fad31191edd366794e56fe7f549f
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-12-29 03:29:28 +0200

    write regex for email

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
-export const REGEX_EMAIL = /^[a-zA-Z\d._-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$|^\s*$/;
+export const REGEX_EMAIL =
+  /^[a-zA-Z\d._-]{2,}@[a-zA-Z\d.-]+(?!\.ru|\.by)\.[a-zA-Z]{2,}$|^\s*$/gm;
 export const REGEX_PHONE = /\+380[3-9]\d{8}$/;
 export const REGEX_NAME = /^[a-zA-ZА-ЩЬЮЯҐЄІЇа-щьюяґєії'`’ʼ" -]{2,}|^\s*$/gm;
 
