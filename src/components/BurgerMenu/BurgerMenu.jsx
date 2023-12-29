09030e054e2a836145b02d2c6b0cf1a79c4f714e
Wolodymyr Magera <foxwest@ukr.net>
2023-12-29 02:04:37 +0200

    styles changed

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -2,7 +2,6 @@ import { SectionStyled } from "./BurgerMenu.styled";
 import { Menu } from "../Menu";
 
 import { LangButton } from "../Layout/Layout.styled";
-import { useRef } from "react";
 
 export const BurgerMenu = ({
 	activeLang,
@@ -10,26 +9,8 @@ export const BurgerMenu = ({
 	isOpen,
 	setIsOpen,
 }) => {
-	const ref = useRef();
-<<<<<<< HEAD
-=======
-	useEffect(() => {
-		function ClickOut(e) {
-			if (!ref.current.contains(e.target)) {
-				setIsOpen(prev => !prev);
-			}
-		}
-		document.body.addEventListener("mousedown", ClickOut);
-		return () => {
-			document.removeEventListener("mousedown", ClickOut);
-		};
-	}, []);
->>>>>>> 6f51b0661f4c6f3ed215bd14230982a5bcd6bc69
 	return (
-		<SectionStyled
-			ref={ref}
-			className={`${!isOpen ? "close" : ""}`}
-		>
+		<SectionStyled className={`${!isOpen ? "close" : ""}`}>
 			<Menu
 				setIsOpen={setIsOpen}
 				side={true}
