3caa10af670f200d2ed05cc1b0918238af33bf4d
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-12-31 01:27:29 +0200

    write adaptive information section for 1024 scren

--- src/components/Information/Information.styled.js
+++ src/components/Information/Information.styled.js
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import { fontSectionTitle } from '../../styles/mixins';
+import { fontSectionTitle, device } from '../../styles/mixins';
 
 export const SectionStyled = styled.section`
   background-color: var(--lightBackground);
@@ -7,14 +7,34 @@ export const SectionStyled = styled.section`
 
 export const Container = styled.div`
   margin: 0 auto;
-  padding: 132px 160px;
-  max-width: 1440px;
+
+  @media screen and (${device.tablet}) {
+    padding: 120px 80px;
+  }
+
+  @media screen and (${device.desktop}) {
+    padding: 132px 160px;
+    max-width: 1440px;
+  }
 `;
 
 export const TitleStyled = styled.h2`
-  width: 944px;
   margin-bottom: 40px;
 
   ${fontSectionTitle};
   color: var(--mainText);
+
+  @media screen and (${device.tablet}) {
+    width: 756px;
+
+    font-size: 32px;
+    line-height: calc(45 / 32);
+  }
+
+  @media screen and (${device.desktop}) {
+    width: 944px;
+
+    font-size: 40px;
+    line-height: calc(56 / 40);
+  }
 `;
--- src/components/Information/InformationItem/InformationItem.styled.js
+++ src/components/Information/InformationItem/InformationItem.styled.js
@@ -1,10 +1,18 @@
 import styled from '@emotion/styled';
+import { device } from '../../../styles/mixins';
 
 export const LiStyled = styled.li`
   display: flex;
   flex-direction: ${({ id }) => (id % 2 === 0 ? 'row-reverse' : 'row')};
   justify-content: space-between;
-  min-height: 500px;
+
+  @media screen and (${device.tablet}) {
+    min-height: 430px;
+  }
+
+  @media screen and (${device.desktop}) {
+    min-height: 500px;
+  }
 `;
 
 export const ImageWrp = styled.div`
@@ -12,21 +20,23 @@ export const ImageWrp = styled.div`
 `;
 
 export const ImageStyled = styled.img`
-  width: 548px;
-  height: 388px;
-
   border-radius: 24px;
+
+  @media screen and (${device.tablet}) {
+    width: 428px;
+    height: 292px;
+  }
+
+  @media screen and (${device.desktop}) {
+    width: 548px;
+    height: 388px;
+  }
 `;
 
 export const ShortTextStyled = styled.div`
   position: absolute;
   top: 0;
   left: 0;
-  transform: translate(-72px, 250px);
-
-  padding: 40px 24px;
-  width: 520px;
-  min-height: 212px;
 
   border-radius: 24px;
   background-color: var(--lightText);
@@ -45,22 +55,51 @@ export const ShortTextStyled = styled.div`
 
   p {
     font-family: var(--Raleway);
-    font-size: 18px;
     font-weight: 400;
-    line-height: calc(27 / 18);
     letter-spacing: 0em;
     font-style: italic;
     color: var(--mainText);
+
+    @media screen and (${device.tablet}) {
+      font-size: 16px;
+      line-height: calc(24 / 16);
+    }
+
+    @media screen and (${device.desktop}) {
+      font-size: 18px;
+      line-height: calc(27 / 18);
+    }
+  }
+
+  @media screen and (${device.tablet}) {
+    transform: translate(-40px, 170px);
+    padding: 20px 24px;
+    width: 320px;
+    min-height: 224px;
+  }
+
+  @media screen and (${device.desktop}) {
+    transform: translate(-72px, 250px);
+    padding: 40px 24px;
+    width: 520px;
   }
 `;
 
 export const DescriptionStyled = styled.p`
-  width: 452px;
-
   font-family: var(--PTSans);
-  font-size: 22px;
   font-weight: 400;
-  line-height: calc(35 / 22);
   letter-spacing: 0em;
   color: var(--mainText);
+
+  @media screen and (${device.tablet}) {
+    width: 348px;
+    font-size: 20px;
+    line-height: calc(32 / 20);
+  }
+
+  @media screen and (${device.desktop}) {
+    width: 452px;
+    font-size: 22px;
+    line-height: calc(35 / 22);
+  }
 `;
