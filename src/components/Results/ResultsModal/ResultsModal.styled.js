03f59ee58b02b7352f1eaa046c2b01069a5e381d
Gregory <bjoldgv@gmail.com>
2024-01-03 16:00:03 +0200

    API connection + fix style

--- src/components/Results/ResultsModal/ResultsModal.styled.js
+++ src/components/Results/ResultsModal/ResultsModal.styled.js
@@ -1,5 +1,11 @@
 import styled from "@emotion/styled";
-import { fontSectionTitle,fontLayoutCall, flexBox } from "../../../styles/mixins";
+import {
+	fontSectionTitle,
+	fontLayoutCall,
+	flexBox,
+	fontReview,
+	device,
+} from "../../../styles/mixins";
 import "../../../index.css";
 
 export const MainModalResult = styled.div`
@@ -11,27 +17,58 @@ export const MainModalResult = styled.div`
 	overflow: hidden;
 	background: linear-gradient(0deg, #f1f2f4, #f1f2f4),
 		linear-gradient(0deg, #98a5b1, #98a5b1);
+
+	@media screen and (${device.tablet}) {
+		width: 844px;
+	}
+
+	@media screen and (${device.desktop}) {
+		width: 1080px;
+	}
 `;
 
 export const TitleModal = styled.h4`
 	${fontSectionTitle};
-	font-size: 28px;
 	font-weight: 400;
-	line-height: 42px;
 	letter-spacing: 0em;
 	text-align: center;
 	color: var(--mainText);
+
+	@media screen and (${device.tablet}) {
+		font-size: 18px;
+	}
+
+	@media screen and (${device.desktop}) {
+		font-size: 28px;
+		line-height: 42px;
+	}
 `;
 
 export const ImageModal = styled.img`
 	border-radius: 24px;
-	width: 464px;
 	height: auto;
+
+	@media screen and (${device.tablet}) {
+		width: 348px;
+	}
+
+	@media screen and (${device.desktop}) {
+		width: 464px;
+	}
 `;
 
 export const TextModal = styled.div`
 	${flexBox};
+	${fontReview};
 	color: #000000;
+
+	@media screen and (${device.tablet}) {
+		font-size: 16px;
+	}
+
+	@media screen and (${device.desktop}) {
+		font-size: 18px;
+	}
 `;
 
 export const CtaBox = styled.div`
@@ -39,4 +76,8 @@ export const CtaBox = styled.div`
 	${fontLayoutCall};
 	gap: 24px;
 	color: #969395;
+
+	@media screen and (${device.desktop}) {
+		font-size: 14px;
+	}
 `;
