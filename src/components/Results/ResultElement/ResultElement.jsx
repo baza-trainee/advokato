e14b5c6ce52ac5cb49bf42ca22d8387134653a0c
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2024-01-05 22:10:28 +0200

    fix useEffect w/get query to server, fix reExport components

--- src/components/Results/ResultElement/ResultElement.jsx
+++ src/components/Results/ResultElement/ResultElement.jsx
@@ -1,110 +1,101 @@
-import { useState, useEffect } from "react";
-import { getContent } from "/src/api/fetchContent";
+import { useState, useEffect } from 'react';
 
-import { ModalFromRoot } from "../../ModalFromRoot";
-import ResultsModal from "../ResultsModal/ResultsModal";
+import { getContent } from '../../../api';
+import { ModalFromRoot } from '../../ModalFromRoot';
+import { ResultsModal } from '../ResultsModal';
+import { Icon } from '../../Icon';
 
 import {
-	ContentStyled,
-	WrapperStyled,
-	ResultItemStyled,
-	ImageBlock,
-	ImageStyled,
-	ResultText,
-	BubbleForm,
-	BubbleFormText,
-	PublicationDate,
-	ArrowBlock,
-} from "./ResultElement.styled.js";
-
-import { Icon } from "../../Icon";
+  ContentStyled,
+  WrapperStyled,
+  ResultItemStyled,
+  ImageBlock,
+  ImageStyled,
+  ResultText,
+  BubbleForm,
+  BubbleFormText,
+  PublicationDate,
+  ArrowBlock,
+} from './ResultElement.styled.js';
 
 export const ResultElement = () => {
-	const [modalData, setModalData] = useState(null);
-	const [data, setData] = useState([]);
+  const [modalData, setModalData] = useState(null);
+  const [data, setData] = useState([]);
 
-	useEffect(() => {
-		getContent("https://advocato-backend.vercel.app/api/v1/news").then(res =>
-			setData(res),
-		);
-	}, []);
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getContent('news');
 
-	useEffect(() => {
-		if (!modalData) {
-			document.body.style.overflowY = "auto";
-		}
-	}, [modalData]);
+      if (data?.length > 0) {
+        setData(data);
+      }
+    };
 
-	const openModalWithSpecificData = selectedData => {
-		document.body.style.overflowY = "hidden";
-		setModalData(selectedData);
-	};
+    getData();
+  }, []);
 
-	const closeModal = () => {
-		document.body.style.overflowY = "auto";
-		setModalData(null);
-	};
+  useEffect(() => {
+    if (!modalData) {
+      document.body.style.overflowY = 'auto';
+    }
+  }, [modalData]);
 
-	return (
-		<>
-			{data?.length > 1 &&
-				data.map(
-					(
-						{
-							id,
-							photo_path,
-							name,
-							description,
-							specialization_name,
-							created_at,
-						},
-						index,
-					) => (
-						<ContentStyled key={id}>
-							<WrapperStyled>
-								<ResultItemStyled>
-									<ImageBlock
-										width={464}
-										height={312}
-									>
-										<ImageStyled
-											src={photo_path}
-											alt="photo-result"
-										/>
-									</ImageBlock>
+  const openModalWithSpecificData = selectedData => {
+    document.body.style.overflowY = 'hidden';
+    setModalData(selectedData);
+  };
 
-									<ResultText>
-										<h3>{name}</h3>
-										<p className="desc">{description}</p>
-										<BubbleForm>
-											<BubbleFormText>{specialization_name}</BubbleFormText>
-											<PublicationDate>{created_at}</PublicationDate>
-										</BubbleForm>
-									</ResultText>
-								</ResultItemStyled>
-							</WrapperStyled>
+  const closeModal = () => {
+    document.body.style.overflowY = 'auto';
+    setModalData(null);
+  };
 
-							<ArrowBlock onClick={() => openModalWithSpecificData(data[index])}>
-								<Icon
-									id={"icon-arrow-up"}
-									width={14}
-									height={12}
-								/>
-							</ArrowBlock>
-							{modalData && (
-								<ModalFromRoot
-									toggleModal={closeModal}
-									align={"flex-start"}
-								>
-									<ResultsModal
-										data={modalData}
-										setModalActive={closeModal}
-									/>
-								</ModalFromRoot>
-							)}
-						</ContentStyled>
-					),
-				)}
-		</>
-	);
+  return (
+    <>
+      {data?.length > 1 &&
+        data.map(
+          (
+            {
+              id,
+              photo_path,
+              name,
+              description,
+              specialization_name,
+              created_at,
+            },
+            index
+          ) => (
+            <ContentStyled key={id}>
+              <WrapperStyled>
+                <ResultItemStyled>
+                  <ImageBlock width={464} height={312}>
+                    <ImageStyled src={photo_path} alt="photo-result" />
+                  </ImageBlock>
+
+                  <ResultText>
+                    <h3>{name}</h3>
+                    <p className="desc">{description}</p>
+                    <BubbleForm>
+                      <BubbleFormText>{specialization_name}</BubbleFormText>
+                      <PublicationDate>{created_at}</PublicationDate>
+                    </BubbleForm>
+                  </ResultText>
+                </ResultItemStyled>
+              </WrapperStyled>
+
+              <ArrowBlock
+                onClick={() => openModalWithSpecificData(data[index])}
+              >
+                <Icon id={'icon-arrow-up'} width={14} height={12} />
+              </ArrowBlock>
+              {modalData && (
+                <ModalFromRoot toggleModal={closeModal} align={'flex-start'}>
+                  <ResultsModal data={modalData} setModalActive={closeModal} />
+                </ModalFromRoot>
+              )}
+            </ContentStyled>
+          )
+        )}
+    </>
+  );
 };
