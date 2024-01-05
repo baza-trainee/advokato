3a2fcae4bfcb6b02d1349d9c560f94b683f1cac9
Alexey Yermachkov <o.yermachkov@gmail.com>
2024-01-05 23:20:10 +0200

    update scroll-up-button styles

--- src/components/ScrollUpButton/ScrollUpButton.styled.js
+++ src/components/ScrollUpButton/ScrollUpButton.styled.js
@@ -1,28 +1,36 @@
-import styled from '@emotion/styled';
-import { Icon } from '../Icon';
-import { device } from '../../styles/mixins';
+import styled from "@emotion/styled";
+import { Icon } from "../Icon";
+import { device } from "../../styles/mixins";
 
 export const IconClose = styled(Icon)`
-  position: fixed;
-  z-index: 150;
-  right: 20px;
-  bottom: 48px;
+	position: fixed;
+	z-index: 150;
+	right: 20px;
+	bottom: 48px;
 
-  width: 32px;
-  height: 32px;
+	width: 32px;
+	height: 32px;
 
-  cursor: pointer;
+	cursor: pointer;
 
-  @media screen and (min-width: 768px) {
-    right: 16px;
-    bottom: 56px;
-  }
+	@media screen and (${device.mobileL}) {
+		right: 16px;
+		bottom: 35px;
+	}
 
-  @media screen and (${device.tablet}) {
-    right: 40px;
-    bottom: 88px;
+	@media screen and (${device.tablet}) {
+		right: 12px;
+		bottom: 20px;
 
-    width: 80px;
-    height: 80px;
-  }
+		width: 48px;
+		height: 48px;
+	}
+
+	@media screen and (${device.desktop}) {
+		right: 40px;
+		bottom: 48px;
+
+		width: 80px;
+		height: 80px;
+	}
 `;
