64f3d7662696667fad615f4908d3595b479fb650
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2024-01-06 12:19:52 +0200

    start adaptive styles for messages feedback form

--- src/components/FeedBackForm/SuccessPage/SuccessPage.styled.js
+++ src/components/FeedBackForm/SuccessPage/SuccessPage.styled.js
@@ -1,23 +1,45 @@
 import styled from '@emotion/styled';
-import { fontSectionTitle } from '../../../styles/mixins';
+import { fontSectionTitle, device } from '../../../styles/mixins';
 
 export const DivStyled = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
 
-  width: calc(98px * 2 + 404px);
   background-color: var(--lightBackground);
 
-  h2 {
-    margin-top: 84px;
-    margin-bottom: 20px;
-    height: 48px;
-    min-width: 180px;
+  @media screen and (${device.mobileL}) {
+    padding-top: 48px;
+    padding-bottom: 32px;
+    width: 400px;
+  }
 
+  @media screen and (${device.tablet}) {
+    padding-top: 84px;
+    width: 600px;
+  }
+
+  h2 {
     ${fontSectionTitle};
     text-align: center;
     color: var(--mainText);
+
+    @media screen and (${device.mobileL}) {
+      margin-bottom: 20px;
+      height: 36px;
+      min-width: 120px;
+
+      font-size: 24px;
+      line-height: calc(34 / 24);
+    }
+
+    @media screen and (${device.tablet}) {
+      height: 48px;
+      min-width: 180px;
+
+      font-size: 40px;
+      line-height: calc(56 / 40);
+    }
   }
 
   p {
@@ -39,6 +61,6 @@ export const DivStyled = styled.div`
 
   svg {
     fill: var(--trueText);
-    margin-bottom: 32px;
+    /* margin-bottom: 32px; */
   }
 `;
