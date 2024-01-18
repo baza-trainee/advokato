c20dbfcd5a7cc4bac805a4daf5c9d8e5170d54f5
slepec <slepec2018@gmail.com>
2024-01-18 12:13:04 +0000

    fix map and 404

--- src/components/Contacts/GoogleMap/GoogleMap.styled.js
+++ src/components/Contacts/GoogleMap/GoogleMap.styled.js
@@ -30,10 +30,40 @@ export const containerStyleMobileSPlus = {
   height: '200px',
 };
 
-export const MarkerWrp = styled.div`
-  padding: 10px;
-  width: clamp(200px, 40%, 300px);
+// export const MarkerWrp = styled.div`
+//   padding: 10px;
+//   width: clamp(200px, 40%, 300px);
 
-  color: var(--lightText);
-  background-color: var(--darkBackground);
+//   color: var(--lightText);
+//   background-color: var(--darkBackground);
+// `;
+
+export const LeafletContainer = styled.div`
+width: 548px;
+height: 600px;
+
+@media (max-width: 1440px) {
+  width: 420px;
+  height: 348px;
+}
+
+@media (max-width: 1024px) {
+  width: 648px;
+  height: 408px;
+}
+
+@media (max-width: 768px) {
+  width: 400px;
+  height: 300px;
+}
+
+@media (max-width: 500px) {
+  width: 372px;
+  height: 200px;
+}
+
+@media (max-width: 375px) {
+  width: 272px;
+  height: 140px;
+}
 `;
